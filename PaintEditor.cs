91e4f962d8f4f7d3e126dc9e47ba1c14b6ffac49
MatsuuraAkase <roi.akase@outlook.jp>
2015-09-23 21:53:04 +0900

    スクリプトの更新
    ・コメントの追加
    ・ペンツールで描けなくなるバグ修正

--- PaintEditor.cs
+++ PaintEditor.cs
@@ -5,43 +5,65 @@ using System.Collections;
 
 public class PaintEditor : EditorWindow
 {
+
+	//ウィンドウ座標
+	private const float WINDOW_POS_X = 300.0f;
+	private const float WINDOW_POS_Y = 50.0f;
 	private const float WINDOW_W = 400.0f;
-	private const float WINDOW_H = 670.0f;
+	private const float WINDOW_H = 690.0f;
+
+	//ドット絵描画座標
 	private const float OFFSET_POS_X = 10;
-	private const float OFFSET_POS_Y = 270;
+	private const float OFFSET_POS_Y = 300;
+	
+	//一辺の最大値
 	private const float MAX_SIDE = 380;
 
-	public static int width = 8;
-	public static int height = 8;
-	public static int xx = 0;
-	public static int yy = 0;
-
+	//ウィンドウ
 	public static PaintEditor window;
 	public static Rect[,] rectList = new Rect[64, 64];
 	public static Color[,] rectColor = new Color[64, 64];
-
+	
+	//出力する時のファイル名
 	public static string imgName = "ImageName";
+	
+	//ドット数の高さと幅の初期値
+	public static int width = 8;
+	public static int height = 8;
+
+	//マウスの座標
 	private Vector2 mousePos = Vector2.zero;
-	private Vector2 rectPos = Vector2.zero;
-	private Event action;
+
+	//現在選択している色
 	private Color nowColor = Color.black;
-	private Color color1 = Color.black;
-	private Color color2 = Color.white;
 
-	public static Color[] glidColor = new Color[3];
+	//パレット
+	private Color pallet1 = Color.black;
+	private Color pallet2 = Color.blue;
+	private Color pallet3 = Color.white;
+
 	private Texture2D whiteTexture = Texture2D.whiteTexture;
-
-	private GlidLine glidLine = GlidLine.Black;
+	private GlidLineStr glidLineStr = GlidLineStr.Black;
 	private Tools tools = Tools.Pen;
 	private ExportType exportType = ExportType.PNG;
+	
+	// グリッド線の色
+	private Color[] glidColor = new Color[]
+	{
+		Color.clear,
+		Color.black,
+		Color.white
+	};
 
-	public enum GlidLine
+	// グリッド線の文字列
+	public enum GlidLineStr
 	{
 		None,
 		Black,
 		White,
 	}
 
+	// ツールリスト
 	public enum Tools
 	{
 		Pen,
@@ -49,26 +71,26 @@ public class PaintEditor : EditorWindow
 		Backet,
 	}
 
+	// 出力形式
 	public enum ExportType
 	{
 		PNG,
 		JPEG
 	}
 
-	[MenuItem("Tools/DotPaintTool")]
+	//ペイントツールを開く
+	[MenuItem("Tools/DotPaintTool/PaintTool")]
 	public static void OpenPaintTool()
 	{
 		Init();
 		window = EditorWindow.GetWindow<PaintEditor>("PaintTool");
-		window.position = new Rect(300, 30, WINDOW_W, WINDOW_H);
+		window.position = new Rect(WINDOW_POS_X, WINDOW_POS_Y, WINDOW_W, WINDOW_H);
 		window.Show();
 	}
+
+	//ペイントツールを開くときの初期化
 	public static void Init()
 	{
-		glidColor[0] = Color.clear;
-		glidColor[1] = Color.black;
-		glidColor[2] = Color.white;
-
 		for (int i = 0; i < width; i++)
 		{
 			for (int j = 0; j < height; j++)
@@ -80,7 +102,6 @@ public class PaintEditor : EditorWindow
 	{
 		EditorGUILayout.Space();
 		EditorGUILayout.LabelField("CanvasSize", EditorStyles.boldLabel);
-		
 		EditorGUILayout.BeginHorizontal();
 		EditorGUILayout.BeginVertical();
 		EditorGUILayout.LabelField("Width  : " + width.ToString("00"));
@@ -90,35 +111,60 @@ public class PaintEditor : EditorWindow
 		EditorGUILayout.Space();
 		if (GUILayout.Button("NewImage"))
 		{
-			CreateNewImage.CreateWindow();
+			if (UnityEditor.EditorUtility.DisplayDialog("Notice", "Do you , but you really want disappear ?", "Yes", "No"))
+			{
+				CreateNewImage.CreateWindow();
+			}
 		}
+
+		if (GUILayout.Button("AllDelete"))
+		{
+			Init();
+		}
+
 		EditorGUILayout.EndVertical();
 		EditorGUILayout.EndHorizontal();
 		EditorGUILayout.Space();
 
 		EditorGUILayout.LabelField("Color", EditorStyles.boldLabel);
+
+		EditorGUILayout.BeginVertical();
+
+		nowColor = EditorGUILayout.ColorField("NowColor", nowColor);
+
+		//パレット
 		EditorGUILayout.BeginHorizontal();
-		EditorGUILayout.BeginVertical();
-		color1 = EditorGUILayout.ColorField("NowColor", color1);
-		color2 = EditorGUILayout.ColorField("SubColor", color2);
-		EditorGUILayout.EndVertical();
-		EditorGUILayout.BeginVertical();
+		pallet1 = EditorGUILayout.ColorField("Color1", pallet1);
 		if (GUILayout.Button("Select"))
-			nowColor = color1;
+			nowColor = pallet1;
+		EditorGUILayout.EndHorizontal();
+		EditorGUILayout.BeginHorizontal();
+		pallet2 = EditorGUILayout.ColorField("Color2", pallet2);
 		if (GUILayout.Button("Select"))
-			nowColor = color2;
+			nowColor = pallet2;
+		EditorGUILayout.EndHorizontal();
+		EditorGUILayout.BeginHorizontal();
+		pallet3 = EditorGUILayout.ColorField("Color3", pallet3);
+		if (GUILayout.Button("Select"))
+			nowColor = pallet3;
+		EditorGUILayout.EndHorizontal();
 
 		EditorGUILayout.EndVertical();
-		EditorGUILayout.EndHorizontal();
 
 		EditorGUILayout.Space();
 		GUILayout.Label("GridLine", EditorStyles.boldLabel);
-		glidLine = (GlidLine)GUILayout.Toolbar((int)glidLine, new string[] { "None", "Black", "White" });
+		glidLineStr = (GlidLineStr)GUILayout.Toolbar((int)glidLineStr, new string[] { "None", "Black", "White" });
 		tools = (Tools)GUILayout.Toolbar((int)tools, new string[] { "Pen", "Eraser", "Baket" });
 		EditorGUILayout.Space();
+
+
 		EditorGUILayout.LabelField("ExprotSetting", EditorStyles.boldLabel);
 		EditorGUILayout.BeginHorizontal();
+
+		//出力形式を選択
 		exportType = (ExportType)EditorGUILayout.EnumPopup(exportType);
+
+		//出力ボタンが押されたとき
 		if (GUILayout.Button("Export"))
 			ExportImg();
 		EditorGUILayout.EndHorizontal();
@@ -135,7 +181,7 @@ public class PaintEditor : EditorWindow
 		{
 			for (int j = 0; j < height; j++)
 			{
-				var rect = new Rect(new Vector2((i * side) + OFFSET_POS_X, (j * side) + OFFSET_POS_Y), new Vector2(side, side));
+				var rect = new Rect((i * side) + OFFSET_POS_X, (j * side) + OFFSET_POS_Y, side, side);
 				rectList[i, j] = rect;
 				GUI.color = rectColor[i, j];
 				GUI.DrawTexture(rect, whiteTexture);
@@ -144,35 +190,38 @@ public class PaintEditor : EditorWindow
 
 		for (int i = 0; i <= width; i++)
 		{
-			Handles.color = glidColor[(int)glidLine];
+			Handles.color = glidColor[(int)glidLineStr];
 			Handles.DrawLine(new Vector2((i * side) + OFFSET_POS_X, OFFSET_POS_Y),
 							 new Vector2((i * side) + OFFSET_POS_X, (side * height) + OFFSET_POS_Y));
 		}
 		for (int i = 0; i <= height; i++)
 		{
-			Handles.color = glidColor[(int)glidLine];
+			Handles.color = glidColor[(int)glidLineStr];
 			Handles.DrawLine(new Vector2(OFFSET_POS_X, (i * side) + OFFSET_POS_Y),
 							 new Vector2((side * width) + OFFSET_POS_X, (i * side) + OFFSET_POS_Y));
 		}
 
 
 		wantsMouseMove = true;
-		action = Event.current;
-		mousePos = action.mousePosition;
-
+		var action = Event.current;
+		
 		if (action.type == EventType.MouseDrag ||
 			action.type == EventType.MouseDown)
 		{
+			mousePos = action.mousePosition;
+
+			var xx = 0;
+			var yy = 0;
 
 			if (mousePos.x > (side * width) + OFFSET_POS_X ||
 				mousePos.x < OFFSET_POS_X)
 				xx = -1;
+
 			if (mousePos.y > (side * height) + OFFSET_POS_Y ||
 				mousePos.y < OFFSET_POS_Y)
 				yy = -1;
 
-			if (xx == -1 || yy == -1) { }
-			else
+			if (xx != -1 && yy != -1)
 			{
 
 				int ii = width - 1;
@@ -249,9 +298,9 @@ public class PaintEditor : EditorWindow
 	}
 }
 
-	/// <summary>
-	///	幅と高さを決めるウィンドウ
-	/// </summary>
+/// <summary>
+///	幅と高さを決めるウィンドウ
+/// </summary>
 public class CreateNewImage : EditorWindow
 {
 	private const float WINDOW_W = 250.0f;
@@ -259,10 +308,13 @@ public class CreateNewImage : EditorWindow
 
 	public static CreateNewImage window;
 
-	[MenuItem("Tools/NewImage")]
+	/// <summary>
+	/// 新しくドット絵を編集するとき
+	/// </summary>
+	[MenuItem("Tools/DotPaintTool/NewImage")]
 	public static void CreateWindow()
 	{
-		window = EditorWindow.GetWindow<CreateNewImage>("NEW WINDOW");
+		window = EditorWindow.GetWindow<CreateNewImage>("NEW IMAGE");
 		window.position = new Rect(
 			(Screen.width),
 			(Screen.height),
@@ -277,7 +329,7 @@ public class CreateNewImage : EditorWindow
 	{
 		GUILayout.Space(10);
 		GUILayout.Label("CREATE NEW IMAGE", EditorStyles.boldLabel);
-		PaintEditor.imgName = EditorGUILayout.TextField("ImageName",PaintEditor.imgName);
+		PaintEditor.imgName = EditorGUILayout.TextField("ImageName", PaintEditor.imgName);
 		PaintEditor.width = Mathf.Clamp(EditorGUILayout.IntField("WIDTH ", PaintEditor.width), 1, 64);
 		PaintEditor.height = Mathf.Clamp(EditorGUILayout.IntField("HEIGHT", PaintEditor.height), 1, 64);
 		GUILayout.Space(20);
